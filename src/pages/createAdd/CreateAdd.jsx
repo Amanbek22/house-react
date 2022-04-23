ad83cd5fc4b34e1410b3b33d5cb8887f8eef08f4
Amanbek Asylbekov <een9.aman@gmail.com>
2022-04-23 19:37:28 +0600

    feat: create

--- src/pages/createAdd/CreateAdd.jsx
+++ src/pages/createAdd/CreateAdd.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateAdd() {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  const navigate = useNavigate()
 
   const submit = (e) => {
     e.preventDefault();
@@ -9,27 +17,42 @@ export default function CreateAdd() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        title: "Title",
-        description: "Description",
-        price: "500",
+        title: title,
+        description: description,
+        price: price,
         image: "https://cdn.pixabay.com/photo/2020/03/14/13/25/house-4930614_1280.jpg"
       })
     })
+      .then((res) => {
+        if (res.status === 201) {
+          alert("Success")
+          navigate("/")
+        }
+      })
   }
 
   return (
     <form onSubmit={submit} className="container mt-5">
       <div className="mb-3">
         <label className="form-label">Title</label>
-        <input type="text" className="form-control" placeholder="Title" />
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text" className="form-control" placeholder="Title" />
       </div>
       <div className="mb-3">
         <label className="form-label">Price</label>
-        <input type="text" className="form-control" placeholder="Price" />
+        <input
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          type="text" className="form-control" placeholder="Price" />
       </div>
       <div className="mb-3">
         <label className="form-label">Description</label>
-        <textarea className="form-control" rows="3"></textarea>
+        <textarea 
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+         className="form-control" rows="3"></textarea>
       </div>
       <button type="submit" className="btn btn-success w-100"> + Создать</button>
     </form>
